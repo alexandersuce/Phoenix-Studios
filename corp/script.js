727d2bd1d5f2fbb2fdf9d961773251fc2df17555
Devnest-28 <143426420+alexandersuce@users.noreply.github.com>
2024-09-21 14:20:27 +0200

    Add files via upload

---
+++ corp/script.js
@@ -0,0 +1,11 @@
+document.getElementById('play-wof').addEventListener('click', function() {
+    alert('chargement...');
+    // Redirection vers la page du jeu
+    window.location.href = 'wof.html';
+});
+
+document.getElementById('packs-rewards').addEventListener('click', function() {
+    alert('Redirection vers les packs et récompenses...');
+    // Redirection vers la page des packs et récompenses
+    window.location.href = 'packs.html';
+});
